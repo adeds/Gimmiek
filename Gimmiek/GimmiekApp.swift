68c365deb65d0d840ed07d99555e64d42aaa067f
adesamp <ade@sampingan.co.id>
2021-08-14 21:45:43 +0700

    Initial Commit

---
+++ Gimmiek/GimmiekApp.swift
@@ -0,0 +1,17 @@
+//
+//  GimmiekApp.swift
+//  Gimmiek
+//
+//  Created by Ade Dyas  on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GimmiekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
