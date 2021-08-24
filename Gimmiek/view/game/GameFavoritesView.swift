d5664e1558c0b2e185c40c92b9125260ce45566d
adesamp <ade@sampingan.co.id>
2021-08-25 01:34:10 +0700

    fav empty view

--- Gimmiek/view/game/GameFavoritesView.swift
+++ Gimmiek/view/game/GameFavoritesView.swift
@@ -12,33 +12,45 @@ struct GameFavoritesView: View {
     @State var games = [GameUiModel]()
     var provider: GameDataProvider = { return GameDataProvider() }()
     
-
+    
     var body: some View {
-        List {
-            ForEach(games, id: \.uuid) { (game: GameUiModel) in
-                NavigationLink(
-                    destination: GameDetailView(game: game)
-                        .navigationBarTitle(game.name)) {
-                    GameItemView(game: game)
+        ZStack {
+            
+            List {
+                ForEach(games, id: \.uuid) { (game: GameUiModel) in
+                    NavigationLink(
+                        destination: GameDetailView(game: game)
+                            .navigationBarTitle(game.name)) {
+                        GameItemView(game: game)
+                    }
+                    .padding(EdgeInsets(top: 150, leading: 0, bottom: 0, trailing: 0))
+                    .background(
+                        WebImage(url: URL(string: game.backgroundImage))
+                            .resizable()
+                            .placeholder(Image("image_not_found"))
+                            .indicator(.activity)
+                            .transition(.fade(duration: 0.5))
+                    )
+                    .cornerRadius(10)
+                    
                 }
-                .padding(EdgeInsets(top: 150, leading: 0, bottom: 0, trailing: 0))
-                .background(
-                    WebImage(url: URL(string: game.backgroundImage))
-                        .resizable()
-                        .placeholder(Image("image_not_found"))
-                        .indicator(.activity)
-                        .transition(.fade(duration: 0.5))
-                )
-                .cornerRadius(10)
+                .listRowBackground(SwiftUI.Color.clear)
                 
             }
-            .listRowBackground(SwiftUI.Color.clear)
-        }.onAppear(){
+            
+            VStack {
+                EmptyStateView(isLoading: false, showEmptyState: games.isEmpty)
+                    .frame(
+                        minWidth: 100, idealWidth: 300, maxWidth: 400,
+                        minHeight: 100, idealHeight: 300, maxHeight: 400,
+                        alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+        }
+        .onAppear(){
             provider.getAllFavorites(){ list in
                 games.removeAll()
                 games.append(contentsOf: list)
             }
         }.background(Image("game_bg").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/))
-            
     }
 }
